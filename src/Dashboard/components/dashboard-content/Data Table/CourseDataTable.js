f3a1622d3f0f8d762d12fb7a966ce8dad1bce64b
Abd Alkader Morjan <77734227+abdalkadermorgan@users.noreply.github.com>
2022-06-10 17:20:01 +0300

    api post

--- src/Dashboard/components/dashboard-content/Data Table/CourseDataTable.js
+++ src/Dashboard/components/dashboard-content/Data Table/CourseDataTable.js
@@ -14,7 +14,7 @@ const CourseDataTable = () => {
 	const handleCloseDel = () => setShowDel(false);
 	const handleShowDel = () => setShowDel(true);
 
-	const [course, setCourse] = useState([]);
+	const [courses, setCourses] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
 	const [error, setError] = useState(null);
 
@@ -27,21 +27,24 @@ const CourseDataTable = () => {
 				throw new Error ('wrong!')
 			}
 
-			const data = await response.json();
+			const res = await response.json();
 
-			const loadedCourses = [];
+			console.log(res.data.data);
 
-			for(const key in data) {
-				loadedCourses.push({
-					id: key,
-					title: data[key].title,
-					category: data[key].category,
-					price:data[key].price,
-					discount: data[key].discount,
-					description: data[key].description,
-				});
-			}
-			setCourse(loadedCourses);
+			const loadedCourses = res.data.data;
+			
+			// for(const key in data) {
+			// 	loadedCourses.push({
+			// 		id: key,
+			// 		title: data[key].title,
+			// 		category: data[key].category,
+			// 		price:data[key].price,
+			// 		discount: data[key].discount,
+			// 		description: data[key].description,
+			// 	});
+			// }
+			console.log("load =>" , loadedCourses);
+			setCourses(loadedCourses);
 			
 		} catch (error) {
 			setError(error.message);
@@ -53,59 +56,84 @@ const CourseDataTable = () => {
 		fetchCoursesHandler();
 	}, [fetchCoursesHandler]);
 
+	async function addCourseHandler(course) {
+		const response = await fetch('/api/admin/courses', {
+			method: 'POST',
+			body: JSON.stringify(course),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+		const res = await response.json();
+		console.log(res);
+	}
+
+	let content = <div></div>;
+
+	if (courses.length > 0) {
+		content= "";
+	}
+
+	if (error) {
+	  content = <p>{error}</p>;
+	}
+  
+	if (isLoading) {
+	  content = <p className="" >Loading...</p>;
+	}
 
 
 
 
+	// const { courses } = useSelector((state) => state);
+	// const dispatch = useDispatch();
 
-	const { courses } = useSelector((state) => state);
-	const dispatch = useDispatch();
+	// const [state, setState] = useState({
+	// 	id: "",
+	// 	title: "",
+	// 	description: "",
+	// 	urlImg: "",
+	// 	catigory: "",
+	// 	price: "",
+	// 	discount: "",
+	// });
 
-	const [state, setState] = useState({
-		id: "",
-		title: "",
-		description: "",
-		urlImg: "",
-		catigory: "",
-		price: "",
-		discount: "",
-	});
+	// const onEdit = (course) => {
+	// 	handleShow(true);
+	// 	setState(course);
+	// };
+	// const onEditcourse = () => {
+	// 	dispatch(
+	// 		Actions.SetCourses(
+	// 			courses.map((course) => {
+	// 				if (course.id === state.id) {
+	// 					course = state;
+	// 				}
+	// 				return course;
+	// 			})
+	// 		)
+	// 	);
 
-	const onEdit = (course) => {
-		handleShow(true);
-		setState(course);
-	};
-	const onEditcourse = () => {
-		dispatch(
-			Actions.SetCourses(
-				courses.map((course) => {
-					if (course.id === state.id) {
-						course = state;
-					}
-					return course;
-				})
-			)
-		);
+	// 	setState((s) => ({ title: "", description: "", urlImg: "", catigory: "", price: "", discount: "", id: "" }));
+	// 	stopEditingHandler();
+	// };
 
-		setState((s) => ({ title: "", description: "", urlImg: "", catigory: "", price: "", discount: "", id: "" }));
-		stopEditingHandler();
-	};
+	// const stopEditingHandler = () => {
+	// 	setShow(false);
+	// };
 
-	const stopEditingHandler = () => {
-		setShow(false);
-	};
-
-	const onDeleteCourse = (id) => {
-		dispatch(Actions.SetCourses(courses.filter((e) => e.id !== id)));
-		setShowDel(false);
-	};
+	// const onDeleteCourse = (id) => {
+	// 	dispatch(Actions.SetCourses(courses.filter((e) => e.id !== id)));
+	// 	setShowDel(false);
+	// };
 
 	return (
 
 		<div className="dashboard-content">
 			<div className="d-flex mb-3 justify-content-end">
-				<CoursesAddModal />
+				<CoursesAddModal onAddCourse={addCourseHandler} />
 			</div>
+				{content}
 			<Table responsive hover>
 				<thead>
 					<tr>
@@ -121,26 +149,29 @@ const CourseDataTable = () => {
 				<tbody>
 					{courses.map((course, index) => (
 						<tr key={`course-${index}`}>
-							<td>1</td>
+							
+							<td>{index+1}</td>
 							<td>
 								<img
-									src={course.urlImg}
+									src={course.image_url}
 									width={50}
 									height={50}
 									alt=""
 								/>
 							</td>
 							<td>{course.title}</td>
-							<td>{course.catigory}</td>
+							<td>{course.category}</td>
 							<td>{course.price}</td>
 							<td>{course.discount}</td>
 							<td>
 								<>
-									<Button variant="link primary edit" onClick={() => onEdit(course)}>
+									<Button variant="link primary edit" 
+									// onClick={() => onEdit(course)}
+									>
 										Edit
 									</Button>
 
-									<Modal show={show} onHide={handleClose} animation={false}>
+									{/* <Modal show={show} onHide={handleClose} animation={false}>
 										<Modal.Header closeButton>
 											<Modal.Title>Edit Course</Modal.Title>
 										</Modal.Header>
@@ -226,14 +257,16 @@ const CourseDataTable = () => {
 												Save Changes
 											</Button>
 										</Modal.Footer>
-									</Modal>
+									</Modal> */}
 								</>
 								<>
-									<Button variant="link danger del" onClick={handleShowDel}>
+									<Button variant="link danger del" 
+									// onClick={handleShowDel}
+									>
 										Del
 									</Button>
 
-									<Modal show={showDel} onHide={handleCloseDel} animation={false}>
+									{/* <Modal show={showDel} onHide={handleCloseDel} animation={false}>
 										<Modal.Header closeButton>
 											<Modal.Title>Delete Course</Modal.Title>
 										</Modal.Header>
@@ -248,7 +281,7 @@ const CourseDataTable = () => {
 												Delete
 											</Button>
 										</Modal.Footer>
-									</Modal>
+									</Modal> */}
 								</>
 							</td>
 						</tr>
--- src/Dashboard/modals/CoursesAddModal.js
+++ src/Dashboard/modals/CoursesAddModal.js
@@ -1,36 +1,61 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { Button, Modal } from 'react-bootstrap';
 import { Actions } from '../../store/store';
 import { v4 as uuid } from "uuid";
 import { useDispatch, useSelector } from 'react-redux';
 import { Field, Form, Formik } from 'formik';
 
-function CoursesAddModal() {
+const CoursesAddModal = (props) => {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  const [state, setState] = useState({
-    title: "",
-    description: "",
-    urlImg: "",
-    catigory: "",
-    price: "",
-    discount: "",
-  });
+  // const [state, setState] = useState({
+  //   title: "",
+  //   description: "",
+  //   urlImg: "",
+  //   catigory: "",
+  //   price: "",
+  //   discount: "",
+  // });
 
 
-  const { courses } = useSelector((state) => state);
+  // const { courses } = useSelector((state) => state);
 
 
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
-  const addNewCourse = () => {
-    dispatch(Actions.SetCourses([...courses, { ...state, id: uuid() }]));
+  // const addNewCourse = () => {
+  //   dispatch(Actions.SetCourses([...courses, { ...state, id: uuid() }]));
+  //   handleClose();
+  // };
+
+
+  const titleRef = useRef('');
+  const categoryRef = useRef('');
+  const priceRef = useRef('');
+  const discountRef = useRef('');
+  const descriptionRef = useRef('');
+  const imgRef = useRef('');
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const course = {
+      title: titleRef.current.value,
+      category: categoryRef.current.value,
+      price: priceRef.current.value,
+      discount: discountRef.current.value,
+      description: descriptionRef.current.value,
+      image_id: imgRef.current.value
+    };
+
+    props.onAddCourse(course);
     handleClose();
-  };
+  }
 
+  const initialValues = { title: '', category: '', price: '', discount: '', description: '', image_id: '' }
 
 
   return (
@@ -46,90 +71,95 @@ function CoursesAddModal() {
         <Modal.Body>
           <div>
             <Formik
-              initialValues={{
-                firstName: '',
-                lastName: '',
-                email: '',
-              }}
+              initialValues={initialValues}
               onSubmit={async (values) => {
                 await new Promise((r) => setTimeout(r, 500));
                 alert(JSON.stringify(values, null, 2));
               }}
             >
-              <Form>
-                <Field
+              <Form onSubmit={submitHandler}>
+                <input
                   id="courseTitle"
                   name="cTitle"
                   placeholder="Title"
-                onChange={(e) => setState((s) => ({ ...s, title: e.target.value }))}
-                	value={state.title}
+                  // onChange={(e) => setState((s) => ({ ...s, title: e.target.value }))}
+                  // value={state.title}
+                  ref={titleRef}
                 />
 
-                <Field
+                <input
                   id="category"
                   name="category"
                   placeholder="Category"
-                onChange={(e) =>
-                  setState((s) => ({ ...s, catigory: e.target.value }))
-                }
-                value={state.catigory}
+                  // onChange={(e) =>
+                  //   setState((s) => ({ ...s, catigory: e.target.value }))
+                  // }
+                  // value={state.catigory}
+                  ref={categoryRef}
                 />
 
-                <Field
+                <input
                   id="price"
                   name="price"
                   placeholder="Price"
                   type="number"
-                onChange={(e) => setState((s) => ({ ...s, price: parseInt(e.target.value) }))}
-                value={state.price}
+                  // onChange={(e) => setState((s) => ({ ...s, price: parseInt(e.target.value) }))}
+                  // value={state.price}
+                  ref={priceRef}
                 />
 
-                <Field
+                <input
                   id="discount"
                   name="discount"
                   placeholder="Discount"
                   type="number"
-                onChange={(e) =>
-                	setState((s) => ({ ...s, discount: e.target.value }))
-                }
-                value={state.discount}
+                  // onChange={(e) =>
+                  // 	setState((s) => ({ ...s, discount: e.target.value }))
+                  // }
+                  // value={state.discount}
+                  ref={discountRef}
                 />
 
-                <Field
+                <textarea
                   as="textarea"
                   id="discrption"
                   name="discrption"
                   placeholder="Discription"
                   type="number"
-                onChange={(e) =>
-                	setState((s) => ({ ...s, description: e.target.value }))
-                }
-                value={state.description}
+                  // onChange={(e) =>
+                  // 	setState((s) => ({ ...s, description: e.target.value }))
+                  // }
+                  // value={state.description}
+                  ref={descriptionRef}
 
                 />
 
-                <Field
+                <input
                   id="img"
                   name="img"
                   placeholder="URL Image"
                   type="text"
-                onChange={(e) =>
-                	setState((s) => ({ ...s, urlImg: e.target.value }))
-                }
-                value={state.urlImg}
+                  // onChange={(e) =>
+                  // 	setState((s) => ({ ...s, urlImg: e.target.value }))
+                  // }
+                  // value={state.urlImg}
+                  ref={imgRef}
                 />
+                <Modal.Footer>
+                  <Button variant="secondary" onClick={handleClose}>
+                    Close
+                  </Button>
+                  <Button type='submit' variant="primary"
+                  // onClick={addNewCourse}
+                  >
+                    Save Changes
+                  </Button>
+                </Modal.Footer>
               </Form>
             </Formik>
           </div>
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button type='submit' variant="primary" onClick={addNewCourse}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
+
       </Modal>
     </>
   );
